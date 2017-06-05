80aa96c9b59157657d58305a5f4806688c2f66de
hellikopter <hellikopter@users.noreply.github.com>
2017-06-05 10:14:11 +0100

    settings retrieval for deployment

--- Deployment/ApplicationSettingsRetrieval.ps1
+++ Deployment/ApplicationSettingsRetrieval.ps1
@@ -1,24 +1,16 @@
 <#
 .SYNOPSIS
-Get instrumentation key for use by API app.
+Get settings for API app.
 
 .DESCRIPTION
-Retrieves value of Application Insights' Instrumentation Key and sets task variable.
-
-.PARAMETER OperationsResourceGroupName
-Name of the Resource Group where the Application Insights is.
-
-.PARAMETER ApplicationInsightsName
-Name of the Application Insights.
+Retrieves value IP of API Management and sets task variable.
 
 .NOTES
 This script is for use as a part of deployment in VSTS only.
 #>
 
 Param(
-    [Parameter(Mandatory=$true)] [string] $OperationsResourceGroupName,
-	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
-    [Parameter(Mandatory=$true)] [string] $ApplicationInsightsName
+	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName
 )
 $ErrorActionPreference = "Stop"
 
@@ -26,15 +18,11 @@ function Log([Parameter(Mandatory=$true)][string]$LogText){
     Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
 }
 
-Log "Getting Instrumentation Key"
-$properties=Get-AzureRmResource -ResourceGroupName $OperationsResourceGroupName -ResourceName $ApplicationInsightsName -ExpandProperties | Select-Object Properties -ExpandProperty Properties
-
 Log "Retrives API Management"
 $apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName
 
 Log "Setting variables to use during deployment"
 Log "Instrumentation Key: $($properties.InstrumentationKey)"
-Write-Host "##vso[task.setvariable variable=ApplicationInsightsInstrumentationKey]$($properties.InstrumentationKey)"
 Write-Host "##vso[task.setvariable variable=APIManagementIP]$($apiManagement.StaticIPs[0])"
 
 Log "Job wel done!"
