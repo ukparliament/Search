834673a6363939a0a4bc29989112b9c2d65e6e66
Samu Lang <langsamu@hotmail.com>
2017-11-03 17:42:49 +0000

    Add skeleton process for hints processing and a mechanism to support JSON serialization of extension elements.

--- Parliament.Search.Api/Controllers/SearchController.cs
+++ Parliament.Search.Api/Controllers/SearchController.cs
@@ -2,11 +2,15 @@
 {
     using Library;
     using Microsoft.ApplicationInsights;
+    using Newtonsoft.Json;
     using OpenSearch;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
+    using System.ServiceModel.Syndication;
     using System.Web.Http;
+    using System.Xml.Linq;
 
     public class SearchController : ApiController
     {
@@ -47,13 +51,90 @@
                 telemetry.TrackDependency(string.Format("Search Engine: {0}", engine.GetType().FullName), searchTerms, startTime, timer.Elapsed, responseFeed != null);
             }
 
+            SearchController.ProcessFeed(responseFeed);
+
             telemetry.TrackMetric("TotalResults", responseFeed.TotalResults, new Dictionary<string, string> {
                 { "searchTerms", searchTerms },
                 { "startIndex", startIndex.ToString() },
                 { "count", count.ToString() }
             });
-            
+
             return ResponseMessage(Request.CreateResponse(responseFeed));
         }
+
+        private static void ProcessFeed(Feed responseFeed)
+        {
+            foreach (var item in responseFeed.Items)
+            {
+                SearchController.ProcessItem(item);
+            }
+        }
+
+        private static void ProcessItem(SyndicationItem item)
+        {
+            var uri = item.BaseUri;
+            var hintsExtension = SearchController.ProcessUri(uri);
+
+            item.ElementExtensions.Add(hintsExtension);
+        }
+
+        private static object ProcessUri(Uri uri)
+        {
+            return new HintsWrapper(
+                new[] {
+                    new Hint {
+                        Label = "hint1",
+                        Filter = "filter1" },
+                    new Hint {
+                        Label = "hint2",
+                        Filter = "filter2" }
+                }
+            );
+        }
+    }
+
+    class HintsWrapper : SyndicationElementExtension
+    {
+        public HintsWrapper(IEnumerable<Hint> hints) : base(new XElement("hints"))
+        {
+            this.Hints = hints;
+        }
+
+        [JsonProperty("Contents")]
+        public IEnumerable<Hint> Hints
+        {
+            get
+            {
+                var hintElements = this.XmlData.Elements("hint");
+
+                return hintElements.Select(hintElement => new Hint
+                {
+                    Label = hintElement.Element("Name").Value,
+                    Filter = hintElement.Element("Filter").Value
+                });
+            }
+            private set
+            {
+                var hintElements = value.Select(hint => new XElement("hint", new[] {
+                    new XElement("Name", hint.Label),
+                    new XElement("Filter", hint.Filter) }));
+
+                this.XmlData.Add(hintElements);
+            }
+        }
+
+        private XElement XmlData
+        {
+            get
+            {
+                return this.GetObject<XElement>();
+            }
+        }
+    }
+
+    public class Hint
+    {
+        public string Label;
+        public string Filter;
     }
 }
