3249170cf1f13931325d50596e5c13b51b36b182
Samu Lang <langsamu@hotmail.com>
2018-07-24 16:20:56 +0100

    Removes exception telemetry implementation that's now included by default. Other package updates.

--- Parliament.Search.Api/Global.asax.cs
+++ Parliament.Search.Api/Global.asax.cs
@@ -6,7 +6,6 @@
     using System;
     using System.Configuration;
     using System.Web.Http;
-    using System.Web.Http.ExceptionHandling;
 
     public class Global : System.Web.HttpApplication
     {
@@ -21,8 +20,6 @@
 
             config.Routes.MapHttpRoute("OpenApiDefinition", "openapi.json", new { controller = "OpenApiDefinition" });
             config.Routes.MapHttpRoute("NamedController", "{controller}");
-
-            config.Services.Add(typeof(IExceptionLogger), new AIExceptionLogger());
 
             config.DependencyResolver = new DependencyResolver();
 
